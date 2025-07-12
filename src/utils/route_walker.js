8c22db31802acca47d1c8fced31997c35a72792c
SERDUN <dmitriiserdun@gmail.com>
2025-07-12 14:09:37 +0300

    feat: prevent multiple matches for dynamic paths

--- src/utils/route_walker.js
+++ src/utils/route_walker.js
@@ -7,15 +7,27 @@ export function preloadRoutes() {
 	return walkRoutes(basePath, '', []);
 }
 
+function isDynamicDirectory(entry) {
+	return entry.isDirectory() && entry.name.startsWith('[') && entry.name.endsWith(']');
+}
+
 function walkRoutes(currentFsPath, currentUrlPath, currentParams, rootFile = 'route.js') {
 	const entries = readdirSync(currentFsPath, {withFileTypes: true});
 	const collectedRoutes = [];
+
+	const isFewDynamicDirectory = entries.filter(entry => entry.isDirectory() && isDynamicDirectory(entry)).length > 1;
+
+	console.log(`Found ${entries.length} entries in ${currentFsPath}. Dynamic directories: ${isFewDynamicDirectory}`);
+
+	if (isFewDynamicDirectory) {
+		throw new Error(`Multiple dynamic directories found in ${currentFsPath}. Only one dynamic directory is allowed per level.`);
+	}
 
 	for (const entry of entries) {
 		const fullPath = path.posix.join(currentFsPath, entry.name);
 
 		if (entry.isDirectory()) {
-			const isDynamic = entry.name.startsWith('[') && entry.name.endsWith(']');
+			const isDynamic = isDynamicDirectory(entry);
 			const paramName = isDynamic ? entry.name.slice(1, -1) : null;
 
 			const nextUrlPath = path.posix.join(currentUrlPath, isDynamic ? `:${paramName}` : entry.name);
@@ -26,9 +38,7 @@ function walkRoutes(currentFsPath, currentUrlPath, currentParams, rootFile = 'ro
 
 		if (entry.isFile() && entry.name === rootFile) {
 			collectedRoutes.push({
-				urlPattern: currentUrlPath || '/',
-				filePath: `file://${fullPath}`,
-				params: currentParams,
+				urlPattern: currentUrlPath || '/', filePath: `file://${fullPath}`, params: currentParams,
 			});
 		}
 	}
